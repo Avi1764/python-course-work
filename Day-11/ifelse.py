af0192540879441fd278363292c02d8ea1a71d5c
Avinash <avinashpaleti5@gmail.com>
2026-08-01 15:32:58 +0530

    Add files via upload

---
+++ Day-11/ifelse.py
@@ -0,0 +1,26 @@
+'''
+username = input("username: ")
+password = input("password: ")
+if username == "admin" and password == "avinash123":
+    print("login successful")
+else:
+    print("invalid  credentials")
+    '''
+
+'''
+products = ["laptop", "mobile", "tablet"]
+search = input("search products: ")
+if search in products:
+    print("product found")
+else:
+    print("product not found")
+    '''
+
+
+bill = int(input("enter the bill: "))
+if bill>99:
+   print(bill)
+else:
+    print(bill+30)
+
+
